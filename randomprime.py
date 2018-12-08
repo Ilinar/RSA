4bd0126572f34a4c4643e62b13665af95d706dde
Ilinar <37158814+Ilinar@users.noreply.github.com>
2018-12-08 18:53:18 +0100

    Update randomprime.py

--- randomprime.py
+++ randomprime.py
@@ -1,12 +1,41 @@
+#wersja testowa, wyświetla kroki po drodze
+
+import random
+def test():
+    a = random.randrange(10,20)
+    print ('wylosowano ',a)
+    for i in range(2,a):
+        print ('dzielimy przez ', i)
+        if a % i == 0:
+            test()
+            break
+
+    else:
+        print (a, 'jest pierwsza') 
+        print(type(a))
+        return a
+
+    
+  
+  
+c = test()
+print(c)
+type(c)
+
+
+#wersja zwykła
+
 import random
 
 def randomprime():
-    num = random.randrange(100, 1000)
+    num = random.randrange(10, 20)
     for i in range(2, num):
-        if (num % i) == 0:
+        if num % i == 0:
             randomprime()
-        else:
-            return num
+            break
+
+    else:
+        return num
         
 a = randomprime()
 print(a)
