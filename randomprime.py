9788d9942c450032fe1f46fdd01e1db5e02f9ff6
Ilinar <37158814+Ilinar@users.noreply.github.com>
2018-12-08 10:04:41 +0100

    Update randomprime.py

--- randomprime.py
+++ randomprime.py
@@ -1,12 +1,13 @@
 import random
 
 def randomprime():
-    num = random.randrange(100,1000)
-    for i in range(2,num):
+    num = random.randrange(100, 1000)
+    for i in range(2, num):
         if (num % i) == 0:
             randomprime()
-            break
-    else:
-        print(num)
+        else:
+            return num
         
-randomprime()
+a = randomprime()
+print(a)
+type(a)
