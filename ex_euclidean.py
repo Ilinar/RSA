4a06b5955b8f1dfde8249d14a7aa11787e67003e
Ilinar <37158814+Ilinar@users.noreply.github.com>
2018-12-17 15:31:53 +0100

    Create ex_euclidean.py

---
+++ ex_euclidean.py
@@ -0,0 +1,14 @@
+def ex_euclidean():
+    a = e
+    b = fi
+    u = 1
+    x = 0
+    w = a
+    z = b
+    while w != 0:
+        if w < z:
+            tym = w
+            w = z
+            z = tym        
+    
+    return d
