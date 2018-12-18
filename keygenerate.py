89e494cd48708be212040ae3fd2ae96dcd1ae7b7
Ilinar <37158814+Ilinar@users.noreply.github.com>
2018-12-18 18:42:42 +0100

    Update keygenerate.py

--- keygenerate.py
+++ keygenerate.py
@@ -1,5 +1,7 @@
 import random
+
 def randomprime():
+    """Losowanie liczby pierwszej."""
     global primenum
     primenum= random.randrange(10, 20)
     
@@ -11,6 +13,7 @@ def randomprime():
         return
     
 def diffrentnum():
+    """Ponowne losowanie w przypadku dwóch identycznych liczb pierwszych."""
     global q
     randomprime()
     q = primenum
@@ -18,8 +21,55 @@ def diffrentnum():
         diffrentnum()
     else:
         return
+    
+def euclidean():
+    """Algorytm Euklidesa, losowanie i dopasowywanie liczby by tworzyć parę licz względnie pierwszych."""
+    global num
+    num = random.randrange(3,n)
+    if num % 2 == 0:
+        euclidean()
+    else:
+        if num == fi:
+            euclidean()
+        a = num
+        b = fi
+        while a != b:
+            if a > b:
+                a = a - b
+            elif a < b:
+                b = b - a
+        if a == b == 1:
+            return
+        else:
+            euclidean()
 
+def ex_euclidean():
+    """Rozszerzony algorytm Euklidesa, szukanie odwrotności modulo."""
+    global d
+    a = e
+    b = fi
+    u = 1
+    x = 0
+    w = a
+    z = b
+    while w != 0:
+        if w < z:
+            tym = w
+            w = z
+            z = tym
+            tym = u
+            u = x
+            x = tym
+        k = int(w / z)
+        u = u - (k * x)
+        w = w - (k * z)
+    if x < 0:
+        x = x + b
+    d = x
+    return d
+    
 def keygenerate():
+    """Tworzenie pary kluczy"""
     randomprime()
     global p
     p = primenum
@@ -30,5 +80,13 @@ def keygenerate():
     global n
     n = p * q
     print (fi, n)
+    euclidean()
+    global e
+    e = num
+    ex_euclidean()
+    print('klucz publiczny:', e , n)
+    print('klucz prywatny:', d , n)
+    if e == d:
+        keygenerate()
     
 keygenerate()
